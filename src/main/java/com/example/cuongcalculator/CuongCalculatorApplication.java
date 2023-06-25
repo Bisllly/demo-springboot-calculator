5b0aa80588d581a98116f9f2848c96b069470cc8
Bisllly <cuongnguyen3900@gmail.com>
2023-06-25 21:45:31 +0700

    Initial commit

---
+++ src/main/java/com/example/cuongcalculator/CuongCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuongcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuongCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuongCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cuongcalculator/CuongCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cuongcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuongCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
